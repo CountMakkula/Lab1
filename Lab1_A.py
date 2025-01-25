b68432f1f6e648f9e697146ca36a9160250048c6
Matthew Mason <matthewmason@ufl.edu>
2025-01-24 20:07:29 -0500

    Update Lab1_A.py

--- Lab1_A.py
+++ Lab1_A.py
@@ -1,3 +1,3 @@
 CelsiusTemp=float(input("Enter the temperature in Celsius: "))
 FahrenheitTemp=CelsiusTemp*1.8+32
-print(f"This is {FahrenheitTemp:.1f} degrees Fahrenheit!")
+print(f"That is {FahrenheitTemp:.1f} degrees Fahrenheit!")
